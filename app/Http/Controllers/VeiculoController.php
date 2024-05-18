1ed9775701e3dc4c8adb7dc401d7317f1d92bafa
Igor Salgado <salgado.igor.is@gmail.com>
2024-05-18 08:04:39 -0300

    Feat: implementando CRUD veiculos

---
+++ app/Http/Controllers/VeiculoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Veiculo;
+use Illuminate\Http\Request;
+
+class VeiculoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $veiculos = Veiculo::all();
+        return view('veiculos.index', compact('veiculos'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('veiculos.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'cliente_id' => 'required',
+            'modelo_veiculo' => 'required',
+            'placa_veiculo' => 'required',
+            'ano_veiculo' => 'required',
+            'cor_veiculo' => 'required'
+        ]);
+
+        Veiculo::create($request->all());
+        return redirect()->route('veiculos.index')->with('success', 'Veículo adicionado com sucesso !!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Veiculo $veiculo)
+    {
+        return view('veiculos.show', compact('veiculo'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Veiculo $veiculo)
+    {
+        return view('veiculos.edit', compact('veiculos'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Veiculo $veiculo)
+    {
+        $request->validate([
+            'cliente_id' => 'required',
+            'modelo_veiculo' => 'required',
+            'placa_veiculo' => 'required',
+            'ano_veiculo' => 'required',
+            'cor_veiculo' => 'required'
+        ]);
+
+        $veiculo->update($request->all());
+        return redirect()->route('veiculos.index')->with('success', 'Veículo atualizado com sucesso !!!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Veiculo $veiculo)
+    {
+        $veiculo->delete();
+        return redirect()->route('veiculos.index')->with('success', 'Veiculo removido com sucesso');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
-// use App\Http\Controllers\VeiculosController;
 // use App\Http\Controllers\ServicoController;
 // use App\Http\Controllers\OrdemServicoController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\VeiculoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +34,7 @@ Route::middleware('auth')->group(function () {
 });
 
 Route::resource('clientes', ClienteController::class);
-// Route::resource('veiculos', VeiculosController::class);
+Route::resource('veiculos', VeiculoController::class);
 // Route::resource('servicos', ServicoController::class);
 // Route::resource('ordemservicos', OrdemServicoController::class);
 
