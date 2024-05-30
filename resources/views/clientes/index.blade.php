dc984f248d72635277d77dc34bf8791ac01603d4
Igor Salgado <salgado.igor.is@gmail.com>
2024-05-30 10:58:24 -0300

    Fix: corrigindo views index

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -8,8 +8,8 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="w-full text-gray-900 dark:text-gray-100">
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="w-full text-gray-900 dark:text-gray-100">
                         <a href="{{ route('clientes.create') }}"
                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-3 inline-block sm:mr-2">
                             Novo</a>
