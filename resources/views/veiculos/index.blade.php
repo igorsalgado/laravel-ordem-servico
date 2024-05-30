0cb19e6b1e7f6cfb2d623bbd2cf6345248e17104
Igor Salgado <salgado.igor.is@gmail.com>
2024-05-30 10:56:06 -0300

    Corrigindo views index

--- resources/views/veiculos/index.blade.php
+++ resources/views/veiculos/index.blade.php
@@ -8,7 +8,7 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
+                <div class="w-full text-gray-900 dark:text-gray-100">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <a href="{{ route('veiculos.create') }}"
                             class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-3 inline-block sm:mr-2">
@@ -32,8 +32,8 @@
                                     <tr class="hover:bg-gray-100 dark:hover:bg-gray-800">
                                         <td class="border border-gray-300 px-2 py-2 text-center">
                                             {{ $veiculo->id }}</td>
-                                            <td class="border border-gray-300 px-2 py-2 text-center">
-                                                {{ $veiculo->cliente->nome_cliente }}</td>
+                                        <td class="border border-gray-300 px-2 py-2 text-center">
+                                            {{ $veiculo->cliente->nome_cliente }}</td>
                                         <td class="border border-gray-300 px-2 py-2 text-center">
                                             {{ $veiculo->modelo_veiculo }}</td>
                                         <td class="border border-gray-300 px-2 py-2 text-center">
@@ -48,8 +48,8 @@
                                                 class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 rounded inline-block mb-1 sm:mb-0">Ver</a>
                                             <a href="{{ route('veiculos.edit', $veiculo->id) }}"
                                                 class= "bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-3 rounded inline-block mb-1 sm:mb-0 background">Editar</a>
-                                            <form action="{{ route('veiculos.destroy',   $veiculo->id) }}" method="POST"
-                                                style="display:inline-block;">
+                                            <form action="{{ route('veiculos.destroy', $veiculo->id) }}"
+                                                method="POST" style="display:inline-block;">
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit"
