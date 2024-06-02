db94568ecb8394357b30b921eb5d93f17c91e0c9
Igor Salgado <salgado.igor.is@gmail.com>
2024-06-02 08:54:35 -0300

    Feat: implementando funcionalidade de buscar CEP no cadastro e edição de cliente

--- resources/views/clientes/create.blade.php
+++ resources/views/clientes/create.blade.php
@@ -1,13 +1,13 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
             {{ __('Criar Novo Cliente') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     @if (session('success'))
                         <div class="alert alert-success">
@@ -19,54 +19,56 @@
                         <div class="mb-4">
                             <label for="nome_cliente" class="block text-sm font-medium text-white">Nome</label>
                             <input type="text" name="nome_cliente" id="nome_cliente"
-                                class="mt-1 block w-full text-gray-900" required>
+                                class="block w-full mt-1 text-gray-900" required>
                         </div>
                         <div class="mb-4">
                             <label for="email_cliente" class="block text-sm font-medium text-white">Email</label>
                             <input type="email" name="email_cliente" id="email_cliente"
-                                class="mt-1 block w-full text-gray-900" required>
+                                class="block w-full mt-1 text-gray-900" required>
                         </div>
                         <div class="mb-4">
                             <label for="telefone_cliente" class="block text-sm font-medium text-white">Telefone</label>
                             <input type="text" name="telefone_cliente" id="telefone_cliente"
-                                class="mt-1 block w-full text-gray-900" required>
+                                class="block w-full mt-1 text-gray-900" required>
+                        </div>
+                        <div class="mb-4">
+                            <label for="cep_cliente" class="block text-sm font-medium text-white">CEP</label>
+                            <input type="text" name="cep_cliente" id="cep_cliente"
+                                class="block w-full mt-1 text-gray-900" required>
                         </div>
                         <div class="mb-4">
                             <label for="logradouro_cliente"
                                 class="block text-sm font-medium text-white">Logradouro</label>
                             <input type="text" name="logradouro_cliente" id="logradouro_cliente"
-                                class="mt-1 block w-full text-gray-900" required>
+                                class="block w-full mt-1 text-gray-900" readonly required>
                         </div>
                         <div class="mb-4">
                             <label for="numero_endereco_cliente"
                                 class="block text-sm font-medium text-white">Número</label>
                             <input type="text" name="numero_endereco_cliente" id="numero_endereco_cliente"
-                                class="mt-1 block w-full text-gray-900" required>
+                                class="block w-full mt-1 text-gray-900" required>
                         </div>
                         <div class="mb-4">
                             <label for="bairro_cliente" class="block text-sm font-medium text-white">Bairro</label>
                             <input type="text" name="bairro_cliente" id="bairro_cliente"
-                                class="mt-1 block w-full text-gray-900" required>
+                                class="block w-full mt-1 text-gray-900" readonly required>
                         </div>
                         <div class="mb-4">
                             <label for="cidade_cliente" class="block text-sm font-medium text-white">Cidade</label>
                             <input type="text" name="cidade_cliente" id="cidade_cliente"
-                                class="mt-1 block w-full text-gray-900" required>
+                                class="block w-full mt-1 text-gray-900" readonly required>
                         </div>
                         <div class="mb-4">
                             <label for="estado_cliente" class="block text-sm font-medium text-white">Estado</label>
                             <input type="text" name="estado_cliente" id="estado_cliente"
-                                class="mt-1 block w-full text-gray-900" required>
+                                class="block w-full mt-1 text-gray-900" readonly required>
                         </div>
-                        <div class="mb-4">
-                            <label for="cep_cliente" class="block text-sm font-medium text-white">CEP</label>
-                            <input type="text" name="cep_cliente" id="cep_cliente"
-                                class="mt-1 block w-full text-gray-900" required>
-                        </div>
-                        <button type="submit" class="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-3 rounded inline-block">Salvar</button>
+                        <button type="submit"
+                            class="inline-block px-3 py-1 font-bold text-white bg-green-500 rounded hover:bg-green-700">Salvar</button>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+    <script src="{{ asset('js/buscar_cep.js') }}"></script>
 </x-app-layout>
